1b0f428545160eee5785e4a9980b86d9f3cce152
miguemesen <miguemesen12@gmail.com>
2021-05-11 00:47:23 -0600

    Initial commit

---
+++ src/main/java/tec/fc/app/App.java
@@ -0,0 +1,13 @@
+package tec.fc.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
